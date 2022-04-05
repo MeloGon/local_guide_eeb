b5eed4b9738c5bad0305230d97de140d5e9114d1
MeloGon <troy.esname@gmail.com>
2022-04-05 17:45:06 -0500

    Definidiendo rutas y nombres de pages

--- lib/route/app_pages.dart
+++ lib/route/app_pages.dart
@@ -1,10 +1,37 @@
 import 'package:get/get.dart';
 import 'package:locals_guide_eeb/modules/access/access_binding.dart';
 import 'package:locals_guide_eeb/modules/access/access_page.dart';
+import 'package:locals_guide_eeb/modules/login/login_binding.dart';
+import 'package:locals_guide_eeb/modules/login/login_page.dart';
+import 'package:locals_guide_eeb/modules/otp/otp_binding.dart';
+import 'package:locals_guide_eeb/modules/otp/otp_page.dart';
+import 'package:locals_guide_eeb/modules/register/register_binding.dart';
+import 'package:locals_guide_eeb/modules/register/register_page.dart';
+import 'package:locals_guide_eeb/modules/register_phone/register_phone_binding.dart';
+import 'package:locals_guide_eeb/modules/register_phone/register_phone_page.dart';
 import 'package:locals_guide_eeb/route/app_routes.dart';
 
-class AppPages{
+class AppPages {
   static final List<GetPage> pages = [
-    GetPage(name: AppRoutes.ACCESS, page: ()=> const AccessPage(), binding: AccessBinding() )
+    GetPage(
+        name: AppRoutes.ACCESS,
+        page: () => const AccessPage(),
+        binding: AccessBinding()),
+    GetPage(
+        name: AppRoutes.REGISTERMODE,
+        page: () => const RegisterPage(),
+        binding: RegisterBinding()),
+    GetPage(
+        name: AppRoutes.REGISTERPHONE,
+        page: () => const RegisterPhonePage(),
+        binding: RegisterPhoneBinding()),
+    GetPage(
+        name: AppRoutes.OTP,
+        page: () => const OtpPage(),
+        binding: OtpBinding()),
+    GetPage(
+        name: AppRoutes.LOGIN,
+        page: () => const LoginPage(),
+        binding: LoginBinding()),
   ];
 }
--- lib/route/app_routes.dart
+++ lib/route/app_routes.dart
@@ -1,3 +1,7 @@
-class AppRoutes{
+class AppRoutes {
   static const ACCESS = '/access';
+  static const REGISTERMODE = '/register';
+  static const REGISTERPHONE = '/register_phone';
+  static const OTP = '/otp';
+  static const LOGIN = '/login';
 }
