4ece557d1e61d9c6111331abf5d2f0b3395fd895
MeloGon <troy.esname@gmail.com>
2022-04-11 08:42:14 -0500

    añadiendo rutas y paginas

--- lib/route/app_pages.dart
+++ lib/route/app_pages.dart
@@ -1,8 +1,12 @@
 import 'package:get/get.dart';
 import 'package:locals_guide_eeb/modules/access/access_binding.dart';
 import 'package:locals_guide_eeb/modules/access/access_page.dart';
+import 'package:locals_guide_eeb/modules/activity_admin/activity_admin_binding.dart';
+import 'package:locals_guide_eeb/modules/activity_admin/activity_admin_page.dart';
 import 'package:locals_guide_eeb/modules/admin_menu/admin_menu_binding.dart';
 import 'package:locals_guide_eeb/modules/admin_menu/admin_menu_page.dart';
+import 'package:locals_guide_eeb/modules/categories_admin/categories_admin_binding.dart';
+import 'package:locals_guide_eeb/modules/categories_admin/categories_admin_page.dart';
 import 'package:locals_guide_eeb/modules/locals_admin/locals_admin_binding.dart';
 import 'package:locals_guide_eeb/modules/locals_admin/locals_admin_page.dart';
 import 'package:locals_guide_eeb/modules/login/login_binding.dart';
@@ -13,6 +17,8 @@ import 'package:locals_guide_eeb/modules/register/register_binding.dart';
 import 'package:locals_guide_eeb/modules/register/register_page.dart';
 import 'package:locals_guide_eeb/modules/register_phone/register_phone_binding.dart';
 import 'package:locals_guide_eeb/modules/register_phone/register_phone_page.dart';
+import 'package:locals_guide_eeb/modules/users_admin/users_admin_binding.dart';
+import 'package:locals_guide_eeb/modules/users_admin/users_admin_page.dart';
 import 'package:locals_guide_eeb/route/app_routes.dart';
 
 class AppPages {
@@ -45,5 +51,17 @@ class AppPages {
         name: AppRoutes.LOCALSADMIN,
         page: () => const LocalsAdminPage(),
         binding: LocalsAdminBinding()),
+    GetPage(
+        name: AppRoutes.USERSADMIN,
+        page: () => const UsersAdminPage(),
+        binding: UserAdminBinding()),
+    GetPage(
+        name: AppRoutes.CATEGORIESADMIN,
+        page: () => const CategoriesAdminPage(),
+        binding: CategoriesAdminBinding()),
+    GetPage(
+        name: AppRoutes.ACTIVITYADMIN,
+        page: () => const ActivityAdminPage(),
+        binding: ActivityAdminBinding()),
   ];
 }
--- lib/route/app_routes.dart
+++ lib/route/app_routes.dart
@@ -6,4 +6,7 @@ class AppRoutes {
   static const LOGIN = '/login';
   static const ADMINMENU = '/admin_menu';
   static const LOCALSADMIN = '/locals_admin';
+  static const USERSADMIN = '/users_admin';
+  static const CATEGORIESADMIN = '/categories_admin';
+  static const ACTIVITYADMIN = '/activity_admin';
 }
