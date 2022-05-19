8bcc2e563d3c41c3f84e216270e34c96dbceb687
Kevyn Melo <kevyn.melo@masuno.pe>
2022-05-19 11:47:20 -0500

    se agregan los botones de filtro y del drawer menu

--- lib/modules/user_module/user_maps/user_maps_page.dart
+++ lib/modules/user_module/user_maps/user_maps_page.dart
@@ -24,78 +24,129 @@ class UserMapsPage extends StatelessWidget {
               physics: const NeverScrollableScrollPhysics(),
               child: SizedBox(
                 height: availableHeight,
-                child: SlidingUpPanel(
-                  minHeight: MediaQuery.of(context).size.height * .4,
-                  maxHeight: MediaQuery.of(context).size.height * .89,
-                  collapsed: collapsedContent(context),
-                  panel: Container(
-                    padding: MyDimens.symetricMarginGeneral,
-                    color: Colors.black,
-                    child: Column(
-                      children: [
-                        SizedBox(
-                            width: MediaQuery.of(context).size.width * .5,
-                            child: Image.asset(
-                                'assets/images/logo/foofle-logo.png')),
-                        Container(
-                          decoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius: BorderRadius.circular(20)),
-                          padding: MyDimens.symetricMarginGeneral,
-                          child: ListView(
-                            shrinkWrap: true,
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
+                child: Stack(
+                  children: [
+                    SlidingUpPanel(
+                      minHeight: MediaQuery.of(context).size.height * .4,
+                      maxHeight: MediaQuery.of(context).size.height * .89,
+                      collapsed: collapsedContent(context),
+                      panel: Container(
+                        padding: MyDimens.symetricMarginGeneral,
+                        color: Colors.black,
+                        child: Column(
+                          children: [
+                            SizedBox(
+                                width: MediaQuery.of(context).size.width * .5,
+                                child: Image.asset(
+                                    'assets/images/logo/foofle-logo.png')),
+                            const SizedBox(height: 20),
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.grey,
+                                  borderRadius: BorderRadius.circular(20)),
+                              padding: MyDimens.symetricMarginGeneral,
+                              child: ListView(
+                                shrinkWrap: true,
                                 children: [
-                                  CircleAvatar(
-                                    radius: 5,
-                                    foregroundColor: Colors.pink,
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      CircleAvatar(
+                                        radius: 5,
+                                        foregroundColor: Colors.pink,
+                                      ),
+                                      Text(
+                                        'Comida Nikei',
+                                        style: MyStyles.generalTextStyleWhite,
+                                      ),
+                                      Text(
+                                        'Distancia',
+                                        style: MyStyles.generalTextStyleWhite,
+                                      )
+                                    ],
                                   ),
-                                  Text(
-                                    'Comida Nikei',
-                                    style: MyStyles.generalTextStyleWhite,
-                                  ),
-                                  Text(
-                                    'Distancia',
-                                    style: MyStyles.generalTextStyleWhite,
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Text(
+                                        'KFC',
+                                        style: MyStyles.generalTextStyleWhite,
+                                      ),
+                                      Text('150m',
+                                          style: MyStyles.generalTextStyleWhite)
+                                    ],
                                   )
                                 ],
                               ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  Text(
-                                    'KFC',
-                                    style: MyStyles.generalTextStyleWhite,
-                                  ),
-                                  Text('150m',
-                                      style: MyStyles.generalTextStyleWhite)
-                                ],
-                              )
-                            ],
+                            )
+                          ],
+                        ),
+                      ),
+                      // this is main body now,
+                      // replace by the scaffold body.
+                      body: GoogleMap(
+                        onMapCreated: (controller) {
+                          _.mapController = controller;
+                          _.mapController.setMapStyle(_.mapStyle);
+                        },
+                        // markers: Set.from(_.myMarker!),
+                        // onTap: _.putMarker,
+                        initialCameraPosition: const CameraPosition(
+                          target:
+                              LatLng(-12.050424378417254, -77.04314569048383),
+                          zoom: 12,
+                        ),
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      width: 35,
+                      height: 35,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.black,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(0.0, 1.0), //(x,y)
+                            blurRadius: 6.0,
                           ),
-                        )
-                      ],
+                        ],
+                      ),
+                      child: const Icon(
+                        Icons.menu_rounded,
+                        size: 20,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                  // this is main body now,
-                  // replace by the scaffold body.
-                  body: GoogleMap(
-                    onMapCreated: (controller) {
-                      _.mapController = controller;
-                      _.mapController.setMapStyle(_.mapStyle);
-                    },
-                    // markers: Set.from(_.myMarker!),
-                    // onTap: _.putMarker,
-                    initialCameraPosition: const CameraPosition(
-                      target: LatLng(-12.050424378417254, -77.04314569048383),
-                      zoom: 12,
+                    Positioned(
+                      top: 40,
+                      child: Container(
+                        margin: const EdgeInsets.all(10),
+                        width: 35,
+                        height: 35,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                          border: Border.all(color: Colors.black),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey,
+                              offset: Offset(0.0, 1.0), //(x,y)
+                              blurRadius: 6.0,
+                            ),
+                          ],
+                        ),
+                        child: const Icon(
+                          Icons.filter_list,
+                          size: 20,
+                        ),
+                      ),
                     ),
-                  ),
-                  borderRadius: BorderRadius.circular(10),
+                  ],
                 ),
               )),
         ),
