a24e6cdbc6907770dc565fe1c4fe180a140d5600
Kevyn Melo <kevyn.melo@masuno.pe>
2022-06-03 11:27:54 -0500

    consumo para llenar el tagline y la grilla de imagen

--- lib/modules/access/access_controller.dart
+++ lib/modules/access/access_controller.dart
@@ -1,11 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:locals_guide_eeb/route/app_routes.dart';
 
 class AccessController extends GetxController
     with GetSingleTickerProviderStateMixin {
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
   late AnimationController animationController;
   late Animation<double> animationLogo;
+  String? _tagline, _urlFoto;
+  bool _loading = false;
+  bool get loading => _loading;
+  String? get tagLine => _tagline;
+  String? get urlFoto => _urlFoto;
 
   @override
   void onInit() {
@@ -17,7 +24,26 @@ class AccessController extends GetxController
         .animate(animationController)
       ..addListener(() => update());
     animationController.forward();
+    showResources();
     super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  showResources() async {
+    _loading = true;
+    await firebaseFirestore
+        .collection("GuiaLocales")
+        .doc("recursos")
+        .get()
+        .then((recursos) {
+      _loading = false;
+      _tagline = recursos["tagline"];
+      _urlFoto = recursos["url"];
+    });
   }
 
   void goToRegisterPage() async {
