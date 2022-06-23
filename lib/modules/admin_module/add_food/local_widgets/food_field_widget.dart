8bc336fa08504caf356b1910563e2cf00fd9fffa
MeloGon <troy.esname@gmail.com>
2022-06-22 19:18:21 -0500

    texfield para platos

---
+++ lib/modules/admin_module/add_food/local_widgets/food_field_widget.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:locals_guide_eeb/theme/my_colors.dart';
+import 'package:locals_guide_eeb/theme/my_styles.dart';
+
+class foodFieldWidget extends StatelessWidget {
+  TextEditingController capacityController = TextEditingController();
+  final int? numeroPlato;
+  final String? nombrePlato;
+  final String? idMesa;
+
+  foodFieldWidget({Key? key, this.numeroPlato, this.nombrePlato, this.idMesa})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (nombrePlato != null) {
+      capacityController = TextEditingController(text: nombrePlato);
+    }
+    return SizedBox(
+//      margin: new EdgeInsets.all(8.0),
+      child: ListBody(
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text('Cantidad de personas',
+                  style: MyStyles.generalTextStyleWhite),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Text(
+                    'Plato ${numeroPlato! + 1}',
+                    style: MyStyles.generalTextStyleWhite,
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.fromLTRB(5, 5, 5, 0),
+                      child: TextFormField(
+                        controller: capacityController,
+                        decoration: const InputDecoration(
+                            fillColor: MyColors.white,
+                            filled: true,
+                            border: OutlineInputBorder()),
+                        keyboardType: TextInputType.text,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
